d76009c1f469f1c0f3f25ada03ab7959526f86cb
Tyson Brown <tysonbrown@live.com.au>
2017-03-31 08:28:51 +1000

    Added dependency injection infrastructure

---
+++ UtilityDelta.Dependencies/Setup.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Autofac;
+using UtilityDelta.Domain.Interface;
+
+namespace UtilityDelta.Dependencies
+{
+    public class Setup : IServiceResolver
+    {
+        private readonly ILifetimeScope _scope;
+
+        public Setup()
+        {
+            var builder = new ContainerBuilder();
+
+            //TODO: Register types here
+
+            _scope = builder.Build().BeginLifetimeScope();
+        }
+
+        public T GetService<T>()
+        {
+            return _scope.Resolve<T>();
+        }
+
+        public void Dispose()
+        {
+            _scope.Dispose();
+        }
+    }
+}
---
+++ UtilityDelta.Domain.Interface/IServiceResolver.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UtilityDelta.Domain.Interface
+{
+    public interface IServiceResolver : IDisposable
+    {
+        T GetService<T>();
+    }
+}
